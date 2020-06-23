1b79903cc979be11531409a8447deacd077679e6
Loc Thanh <locnt19@gmail.com>
2020-06-23 17:05:20 +0700

    summary

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,53 +1,55 @@
 document.addEventListener('DOMContentLoaded', function () {
-  var message = document.getElementById('server-message') ? document.getElementById('server-message').textContent : null;
+  var message = document.getElementById('server-message') ? document.getElementById('server-message').textContent : null
   if (message) {
-    console.log('message: ', message);
+    console.log('message: ', message)
     Toastify({
       text: message,
       gravity: 'top',
       position: 'right',
       duration: 3000,
       callback: function () {
-        message = null;
-        // console.log('callback: %s', message);
+        message = null
+        // console.log('callback: %s', message)
       }
-    }).showToast();
-  };
+    }).showToast()
+  }
 
-  const $fakeSelectItem = $('.fake__select .dropdown-item');
+  //#region FAKE SELECT IN REGISTER, USER/ME PAGE
+  const $fakeSelectItem = $('.fake__select .dropdown-item')
   $fakeSelectItem.on('click', function () {
-    const text = $(this).text();
-    $(this).parents('.fake__select').find('input').val(text);
-  });
-  $('#users_me input').attr('readonly', true);
-  $('#users_me button').attr('readonly', true);
-  $('#users_me .fake__select .dropdown-menu').addClass('d-none');
+    const text = $(this).text()
+    $(this).parents('.fake__select').find('input').val(text)
+  })
+  $('#users_me input').attr('readonly', true)
+  $('#users_me button').attr('readonly', true)
+  $('#users_me .fake__select .dropdown-menu').addClass('d-none')
   $('#users_me_update').click(function () {
-    $(this).addClass('d-none');
-    $('#users_me_update_yes').removeClass('d-none');
-    $('#users_me_update_no').removeClass('d-none');
-    $('#users_me .fake__select .dropdown-menu').removeClass('d-none');
-    $('#users_me input').removeAttr('readonly');
-    $('#users_me button').removeAttr('readonly');
+    $(this).addClass('d-none')
+    $('#users_me_update_yes').removeClass('d-none')
+    $('#users_me_update_no').removeClass('d-none')
+    $('#users_me .fake__select .dropdown-menu').removeClass('d-none')
+    $('#users_me input').removeAttr('readonly')
+    $('#users_me button').removeAttr('readonly')
   })
   $('#users_me_update_no').click(function () {
-    $(this).addClass('d-none');
-    $('#users_me .fake__select .dropdown-menu').addClass('d-none');
-    $('#users_me_update_yes').addClass('d-none');
-    $('#users_me_update').removeClass('d-none');
-    $('#users_me input').attr('readonly', true);
-    $('#users_me button').attr('readonly', true);
+    $(this).addClass('d-none')
+    $('#users_me .fake__select .dropdown-menu').addClass('d-none')
+    $('#users_me_update_yes').addClass('d-none')
+    $('#users_me_update').removeClass('d-none')
+    $('#users_me input').attr('readonly', true)
+    $('#users_me button').attr('readonly', true)
   })
   $('#re_password').keyup(function () {
     if ($('#new_password').val() === $(this).val()) {
-      $(this).removeClass('is-invalid').addClass('is-valid');
-      $('#btn_change_password').removeAttr('disabled');
+      $(this).removeClass('is-invalid').addClass('is-valid')
+      $('#btn_change_password').removeAttr('disabled')
     } else {
-      $(this).addClass('is-invalid');
+      $(this).addClass('is-invalid')
     }
   })
+  //#endregion
 
-  // coming soon
+  //#region COMING SOON PAGE
   var comingSoon = {
     end: new Date($('#comingsoon_ok').val()),
     day: $('#comingsoon_day'),
@@ -57,31 +59,45 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   $(window).on('load', function () {
-    comingSoon.end = Math.floor(comingSoon.end / 1000); // Convert UNIX timestamp + calculate remaining time
-    comingSoon.remain = comingSoon.end - Math.floor(Date.now() / 1000);
+    comingSoon.end = Math.floor(comingSoon.end / 1000) // Convert UNIX timestamp + calculate remaining time
+    comingSoon.remain = comingSoon.end - Math.floor(Date.now() / 1000)
     if (comingSoon.remain > 0) {
       comingSoon.ticker = setInterval(function () {
-        // console.log('RUN');
-        comingSoon.remain--;
+        // console.log('RUN')
+        comingSoon.remain--
         if (comingSoon.remain <= 0) {
-          clearInterval(comingSoon.ticker);
-          comingSoon.remain = 0;
-          window.location.href = '/exams'
+          clearInterval(comingSoon.ticker)
+          comingSoon.remain = 0
+          window.location.assign('/exams')
         }
         // Calculate remaining time
-        var secs = comingSoon.remain;
-        var days = Math.floor(secs / 86400); // 1 day = 60 secs * 60 mins * 24 hrs
-        secs -= days * 86400;
-        var hours = Math.floor(secs / 3600); // 1 hr = 60 secs * 60 mins
-        secs -= hours * 3600;
-        var mins = Math.floor(secs / 60); // 1 min = 60 secs
-        secs -= mins * 60;
+        var secs = comingSoon.remain
+        var days = Math.floor(secs / 86400) // 1 day = 60 secs * 60 mins * 24 hrs
+        secs -= days * 86400
+        var hours = Math.floor(secs / 3600) // 1 hr = 60 secs * 60 mins
+        secs -= hours * 3600
+        var mins = Math.floor(secs / 60) // 1 min = 60 secs
+        secs -= mins * 60
         // Update HTML
-        comingSoon.day.text(days);
-        comingSoon.hr.text(hours);
-        comingSoon.min.text(mins);
-        comingSoon.sec.text(secs);
-      }, 1000);
+        comingSoon.day.text(days)
+        comingSoon.hr.text(hours)
+        comingSoon.min.text(mins)
+        comingSoon.sec.text(secs)
+      }, 1000)
     }
   })
-});
+  //#endregion
+
+  //#region SUMMARY PAGE
+  var summaryTime = 10
+  ticker = setInterval(function () {
+    summaryTime--
+    if (summaryTime === 0) {
+      clearInterval(ticker)
+      summaryTime = 0
+      window.location.assign('/exams')
+    }
+    $('#summary_time').text(summaryTime)
+  }, 1000)
+  //#endregion
+})
--- src/routes/exam.js
+++ src/routes/exam.js
@@ -1,16 +1,20 @@
-const router = require('express').Router();
-const examController = require('../controllers/examController');
-const checkTime = require('../middleware/checkTime');
+const router = require('express').Router()
+const examController = require('../controllers/examController')
+const checkTime = require('../middleware/checkTime')
 
 router.route('/')
-  .get(checkTime, examController.templateReady);
+  .get(checkTime, examController.templateReady)
 
 
 router.route('/coming-soon')
-  .get(examController.templateComingSoon);
+  .get(examController.templateComingSoon)
 
 
 router.route('/section-1')
-  .get(checkTime, examController.templateSection1);
+  .get(checkTime, examController.templateSection1)
 
-module.exports = router;
+router.route('/summary')
+  .get(checkTime, examController.templateSummary)
+  .post(checkTime, examController.templateSummary)
+
+module.exports = router
