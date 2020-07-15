cbcbe3a67c2006fb64dbf47e78baa69efdb02782
Loc Thanh <locnt19@gmail.com>
2020-07-15 19:29:37 +0700

    Thêm câu hỏi phần 1

--- src/controllers/questionController.js
+++ src/controllers/questionController.js
@@ -2,12 +2,14 @@ const DeThi = require('../models/DeThi');
 
 
 //#region Phần 1
-exports.templateSection1 = (req, res) => {
-  res.render('admin/section-1.pug')
+exports.templateSection1 = async (req, res) => {
+  const deThi = await DeThi.findOne({ code: 'P01' })
+  res.render('admin/section-1.pug', { deThi: deThi })
 }
 
-exports.templateSection1Create = (req, res) => {
-  res.render('admin/section-1-create.pug')
+exports.templateSection1Create = async (req, res) => {
+  const deThi = await DeThi.findOne({ code: 'P01' })
+  res.render('admin/section-1-create.pug', { deThi: deThi })
 }
 
 exports.createSection1 = async (req, res) => {
@@ -17,22 +19,16 @@ exports.createSection1 = async (req, res) => {
     if (deThiDaTonTai === null) {
       const deThiMoi = new DeThi(req.body)
       await deThiMoi.save()
-      req.flash('message', 'Tạo thành công 1')
-      res.render('admin/section-1-create.pug')
+      req.flash('message', 'Tạo thành công')
+      res.redirect('/questions/section-1/create')
     }
     // nếu tồn tại section-1 thì bổ sung câu hỏi
-    const cauHoiMoi = new DeThi({ questions: req.body.questions })
-    // const hj = await DeThi.findOne({ code: req.body.code })
-    console.log('cauHoiMoi')
-    console.log(cauHoiMoi)
-    const hj = await DeThi.findOneAndUpdate({ code: req.body.code }, cauHoiMoi.questions, { new: true })
-    console.log('hj');
-    console.log(hj);
-    
-    // console.log(req.body.questions)
+    const deThi = await DeThi.findOne({ code: req.body.code })
+    deThi.questions.push(req.body.questions[0])
+    await deThi.save()
     // thông báo tạo thành công
-    // req.flash('message', 'Tạo thành công 2')
-    res.render('admin/section-1-create.pug')
+    req.flash('message', 'Thêm thành công')
+    res.redirect('/questions/section-1/create')
   } catch (error) {
     req.flash('message', error)
     res.render('admin/section-1-create.pug')
