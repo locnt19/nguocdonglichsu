db401c70a9af83957c350cb9dace3ebd151deba2
Loc Thanh <locnt19@gmail.com>
2020-10-27 00:32:01 +0700

    RESTful API

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,8 @@ const userRoutes = require('./user')
 const questionRoutes = require('./question')
 const adminRoutes = require('./admin')
 const examRoutes = require('./exam')
+const User = require('../models/User');
+const BaiThi = require('../models/BaiThi');
 
 const indexController = require('../controllers/indexController');
 
@@ -21,6 +23,53 @@ router.post('/api', (req, res) => {
   console.log(JSON.stringify(req.body))
   res.json(req.body)
 })
+
+// router.get('/api/users', async (req, res) => {
+//   const users = await User.find();
+//   res.json(users);
+// })
+
+// router.put('/api/users/reset-lan-thi/:id', async (req, res) => {
+//   try {
+//     await User.findByIdAndUpdate(req.params.id, {
+//       lanThi: {
+//         luotThi: 2,
+//         phan1: false,
+//         phan2: false,
+//         phan3: false,
+//         phan4: false,
+//       },
+//     });
+//     res.json({ responsiveCode: 200 });
+//   } catch (error) {
+//     res.json(error);
+//   }
+// })
+
+// router.get('/api/users/:id', async (req, res) => {
+//   const users = await User.find({_id: req.params.id});
+//   res.json(users);
+// })
+
+// router.get('/api/bai-thi', async (req, res) => {
+//   const baiThi = await BaiThi.find();
+//   res.json(baiThi);
+// })
+
+// router.get('/api/bai-thi/:id', async (req, res) => {
+//   const baiThi = await BaiThi.find({user: req.params.id});
+//   res.json(baiThi);
+// })
+
+// router.delete('/api/bai-thi/:id', async (req, res) => {
+//   await BaiThi.findByIdAndDelete(req.params.id);
+//   res.json({responsiveCode: 200});
+// })
+
+// router.delete('/api/bai-thi/user/:user', async (req, res) => {
+//   await BaiThi.findOneAndDelete({user: req.params.user});
+//   res.json({responsiveCode: 200});
+// })
 
 router.get('/403', (req, res) => {
   res.render('403.pug', { title: 'Forbidden' })
